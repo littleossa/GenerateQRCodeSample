76c0d6b0a0481084ceff6df6daf4bb1ca5337e5e
littleossa <littleossa@gmail.com>
2022-10-13 05:35:40 +0900

    Initial Commit

---
+++ GenerateQRCodeSample/GenerateQRCodeSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenerateQRCodeSampleApp.swift
+//  GenerateQRCodeSample
+//
+//  Created by 平岡修 on 2022/10/13.
+//
+
+import SwiftUI
+
+@main
+struct GenerateQRCodeSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
